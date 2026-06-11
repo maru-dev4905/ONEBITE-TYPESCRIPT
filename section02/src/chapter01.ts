091c0ced247650d4222c160ce2a2ff2de8878a84
marudev4905 <marudev4905@gmail.com>
2026-06-11 17:09:02 +0900

    원시타입과 리터럴타입

---
+++ section02/src/chapter01.ts
@@ -0,0 +1,34 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = .123;
+let num4: number = -.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// string
+let str1: string = "Hello";
+let str2: string = 'Hello';
+let str3: string = `Hello`;
+let str4: string = `Hello${num1}`;
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined;
+let unde1: undefined = undefined;
+
+// 리터럴 타입
+// 리터럴 -> 값
+let numA: 10 = 10;
+// numA = 12      // Error;
+
+let strA: "Hello" = "Hello";
+
+let boolA: true = true;
+// let boolA: true = false     // Error;
