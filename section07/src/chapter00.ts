b5d365dc93e47528bbea163b745a36e3882fa166
maru-dev4905 <marudev4905@gmail.com>
2026-06-15 19:29:05 +0900

    제네릭 소개

---
+++ section07/src/chapter00.ts
@@ -0,0 +1,24 @@
+/**
+ * 제네릭
+ * - 일반적인 / 포괄적인
+ *
+ * <T> -> 타입 변수
+ */
+
+// 제네릭 함수
+function func<T>(value: T): T {
+  return value;
+}
+
+let num = func(10);
+// num.toUpperCase();
+// num.toFixed();
+if (typeof num === "number") {
+  num.toFixed();
+}
+
+let bool = func(true);
+
+let str = func("string");
+
+let arr = func<[number, number, number]>([1, 2, 3]);
